c61e9c8a48cbfc6bfeca819af764ace8d126ada2
HappyMell <mellisamadrid@hotmail.com>
2019-02-04 17:22:39 +0000

    added accordion to site

--- front-page.php
+++ front-page.php
@@ -1,5 +1,19 @@
+
 <?php get_header(); ?>
-<body>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+ $(document).ready(function () {
+    $('.accordion-toggle').on('click', function(event){
+    	event.preventDefault();
+    	var accordion = $(this);
+    	var accordionContent = accordion.next('#accordion-content');
+    	accordion.toggleClass("open");
+    	accordionContent.slideToggle("slow");
+    	
+    });
+});
+  </script>
 <div class="main-content">
 
     <section id="section-home">
@@ -7,45 +21,44 @@
             <div class="news-content">
                 <div class="news-title">
                 <hr>
+
                <?php 
                $homePageNews = new WP_Query(array(
                 'posts_per_page' => -1,
                 'post_type' => 'news'
                ));
+                
+               while($homePageNews->have_posts()) { 
+            ?>  <div class="accordion-container"> 
 
-               while($homePageNews->have_posts()) {
-                $homePageNews->the_post(); ?>
-                <div class="panels">
-                <li><a href="<?php the_permalink();?>"> <?php the_title();?></a><img class="cross" src="<?php echo THEME_IMG_PATH;?>/cross.png"></li>
-                </div>
-                <div class="reverse-panel">
+               <?php   $homePageNews->the_post(); ?>
+             <li class="heading"><a href="<?php the_permalink();?>"> <?php the_title();?> </a></li>
+            <img class="accordion-toggle"src="<?php echo THEME_IMG_PATH;?>/cross.png">
+        
+
+                <div id="accordion-content">
+
                  <?php 
                 $image = get_field('news_photo');
                 $size = 'full'; // (thumbnail, medium, large, full or custom size)
                     if( $image ) {
                         echo wp_get_attachment_image( $image, $size );
                         }
-                the_content();
-                    ?>
+
+         the_content(); ?> 
+               </div>
+        
                 <hr>
 
              <?php  }
                ?>
-                </div>
-              
 
-
-                    <a class="bracket-button active" href="#">
-                    <span class="tlt"></span>
-                    <span class="outer">
-                        <span class="inner"></span>
-                    </span>
-                    </a>
+            
                 </div>
+              </div>
             </div>
-            </div>
+        </div>
     </section>
-
 </div>
-                    </body>
-         
+                    
+<!-- -->
--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ function band_files() {
     wp_enqueue_script('band_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), false);
     wp_enqueue_style('font-awesome-arial', '//fonts.googleapis.com/css?family=Source+Sans+Pro');
     wp_enqueue_style('band_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('band_animation', '//cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css');
 
 }
 add_action('wp_enqueue_scripts', 'band_files');
